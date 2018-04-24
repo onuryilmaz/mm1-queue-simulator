f53090646533691951b57d57380fa73b34414aa6
Onur Yilmaz <onur.yilmaz@sap.com>
2018-04-24 11:49:43 +0200

    refactoring

--- main.go
+++ main.go
@@ -1,48 +1,49 @@
 package main
 
 import (
-	"log"
-	"github.com/agoussia/godes"
-	"runtime"
 	"flag"
+	"log"
+	"runtime"
+
+	"github.com/agoussia/godes"
 )
 
+// Job holds the properties of a job instance
 type Job struct {
 	ID                  int
 	creationTime        int
 	processStartTime    int
 	processCompleteTime int
-
-	processDuration int
-
-	finished bool
+	processDuration     int
 }
 
+// NewJob creates a new job at the provided time
 func NewJob(time int) *Job {
 	log.Printf("Time: %d | Job created with ID %d", time, time)
 	j := &Job{creationTime: time, ID: time}
 	return j
 }
 
-func (j *Job) startJob(time int, processDuration int) {
+// StartJob starts the job at the provided time with the duration
+func (j *Job) StartJob(time int, processDuration int) {
 
 	j.processStartTime = time
 	j.processDuration = processDuration
 
 	log.Printf("Time: %d | Job (ID: %d) is exiting queue and starting processing | Process Time: %d", time, j.ID, j.processDuration)
-
 }
 
-func (j *Job) isDone(time int) bool {
-	if (j.processStartTime +j.processDuration) == time {
+// IsDone checks whether the job is completed at the provided time
+func (j *Job) IsDone(time int) bool {
+	if (j.processStartTime + j.processDuration) == time {
 		j.processCompleteTime = time
-		j.finished = true
 		log.Printf("Time: %d | Job (ID: %d) completed processing", time, j.ID)
 		return true
 	}
 	return false
 }
 
+// MM1 holds the properties for MM1 simulator
 type MM1 struct {
 	queue     []*Job
 	server    bool
@@ -57,6 +58,7 @@ type MM1 struct {
 	Statistics
 }
 
+// Statistics holds the properties of collected statistics
 type Statistics struct {
 	JobsGenerated int
 	JobsServiced  int
@@ -75,6 +77,7 @@ type Statistics struct {
 	Time int
 }
 
+// Summary calculates the theoretical and simulation values and prints
 func (s *MM1) Summary() {
 
 	mu := float64(1) / float64(s.meanServiceTime)
@@ -87,19 +90,21 @@ func (s *MM1) Summary() {
 	averageResponseTime := 1.0 / (mu * (1 - ro))
 	averageWaitingTimeInQueue := ro / (mu * (1.0 - ro))
 
-	log.Print("Total number of jobs generated: ", s.JobsGenerated, " | Theoretical: ", s.Time/s.meanArrivalTime)
-	log.Print("Total number of jobs serviced: ", s.JobsServiced, " | Theoretical: ", s.Time/s.meanServiceTime)
-	log.Print("Average queue length: ", s.QueueLength/s.Time, " | Theoretical: ", averageNumberOfJobsInQueue)
-	log.Print("Average waiting time in queue: ", s.QueueWaitingTime/(s.JobsServiced), " | Theoretical: ", averageWaitingTimeInQueue)
-	log.Print("Average number of tasks in the system: ", s.TasksInSystem/s.Time, " | Theoretical: ", averageNumberOfJobsInSystem)
-	log.Print("Response time (Average waiting time in the system): ", s.ReponseTimeInSystem/s.JobsGenerated, " | Theoretical: ", averageResponseTime)
-	log.Printf("Server utilization: %f | Theoretical: %f", float64(s.ServerUtilization)/float64(s.Time), ro)
-	log.Print("Idle time for the server: ", s.Time-s.ServerUtilization)
+	log.Println("SUMMARY------------")
+	log.Printf("Total number of jobs generated: %d | Theoretical Maximum: %.2f", s.JobsGenerated, float64(s.Time)/float64(s.meanArrivalTime))
+	log.Printf("Total number of jobs serviced: %d | Theoretical Maximum: %.2f", s.JobsServiced, float64(s.Time)/float64(s.meanServiceTime))
+	log.Printf("Average queue length: %.2f | Theoretical: %.2f", float64(s.QueueLength)/float64(s.Time), averageNumberOfJobsInQueue)
+	log.Printf("Average waiting time in queue: %.2f | Theoretical: %.2f", float64(s.QueueWaitingTime)/float64(s.JobsServiced), averageWaitingTimeInQueue)
+	log.Printf("Average number of tasks in the system: %.2f | Theoretical: %.2f", float64(s.TasksInSystem)/float64(s.Time), averageNumberOfJobsInSystem)
+	log.Printf("Response time (Average waiting time in the system): %.2f | Theoretical: %.2f", float64(s.ReponseTimeInSystem)/float64(s.JobsGenerated), averageResponseTime)
+	log.Printf("Server utilization: %.2f | Theoretical: %.2f", float64(s.ServerUtilization)/float64(s.Time), ro)
+	log.Printf("Idle time for the server: %d | Theoretical: %.2f", s.Time-s.ServerUtilization, float64(s.Time)*(1-ro))
 
-	log.Print("Average arrival time: ", float64(s.ArrivalTime)/float64(s.JobsGenerated))
-	log.Print("Average service time: ", float64(s.ServiceTime)/float64(s.JobsServiced))
+	log.Printf("Average arrival time: %2.f | Expected: %d", float64(s.ArrivalTime)/float64(s.JobsGenerated), s.meanArrivalTime)
+	log.Printf("Average service time: %2.f | Expected: %d", float64(s.ServiceTime)/float64(s.JobsServiced), s.meanServiceTime)
 }
 
+// NewMM1 creates a new simulator instance with the provided parameters
 func NewMM1(meanArrivalTime int, meanServiceTime int, maximumTime int) *MM1 {
 	m := &MM1{}
 	m.meanArrivalTime = meanArrivalTime
@@ -111,11 +116,13 @@ func NewMM1(meanArrivalTime int, meanServiceTime int, maximumTime int) *MM1 {
 	return m
 }
 
+// Enqueue adds the provided job to the queue at the provided time
 func (mm1 *MM1) Enqueue(j *Job, time int) {
 	log.Printf("Time: %d | Job (ID: %d) entering queue", time, j.ID)
 	mm1.queue = append(mm1.queue, j)
 }
 
+// Dequeue removed a job from the queue at the provided time
 func (mm1 *MM1) Dequeue(time int) (*Job, bool) {
 
 	if len(mm1.queue) == 0 {
@@ -129,27 +136,29 @@ func (mm1 *MM1) Dequeue(time int) (*Job, bool) {
 	return j, true
 }
 
+// GenerateServiceTime generates a random service time
 func (mm1 *MM1) GenerateServiceTime() int {
 	lambda := float64(1.0) / float64(mm1.meanServiceTime)
-	log.Println("Generating service time with lambda :", lambda)
-	data := mm1.exponentialGenerator.Get(lambda)
-	integerData := int(data + 0.5)
-	log.Println("Generated service time:", integerData)
-	return integerData
-}
-
-func (mm1 *MM1) GenerateArrivalTime() int {
-	lambda := float64(1.0) / float64(mm1.meanArrivalTime)
-	log.Println("Generating arrival time with lambda :", lambda)
 	data := mm1.exponentialGenerator.Get(lambda)
 	integerData := int(data + 0.5)
 	if integerData == 0 {
 		integerData = 1
 	}
-	log.Println("Generated arrival time:", integerData)
 	return integerData
 }
 
+// GenerateArrivalTime generates a random arrival time
+func (mm1 *MM1) GenerateArrivalTime() int {
+	lambda := float64(1.0) / float64(mm1.meanArrivalTime)
+	data := mm1.exponentialGenerator.Get(lambda)
+	integerData := int(data + 0.5)
+	if integerData == 0 {
+		integerData = 1
+	}
+	return integerData
+}
+
+// Start starts the simulator
 func (mm1 *MM1) Start() {
 
 	nextArrivalTime := mm1.GenerateArrivalTime()
@@ -169,7 +178,7 @@ func (mm1 *MM1) Start() {
 			mm1.Enqueue(j, time)
 
 			// generate random IAT for the next task
-			randomIAT :=  mm1.GenerateArrivalTime()
+			randomIAT := mm1.GenerateArrivalTime()
 			nextArrivalTime += randomIAT
 			mm1.ArrivalTime += randomIAT
 
@@ -187,7 +196,7 @@ func (mm1 *MM1) Start() {
 			if found {
 				mm1.server = true
 				// generate random service time
-				mm1.inProcess.startJob(time, mm1.GenerateServiceTime())
+				mm1.inProcess.StartJob(time, mm1.GenerateServiceTime())
 				mm1.QueueWaitingTime += mm1.inProcess.processStartTime - mm1.inProcess.creationTime
 			}
 
@@ -196,10 +205,10 @@ func (mm1 *MM1) Start() {
 		}
 
 		// if the server has finished serving the task
-		if mm1.inProcess != nil && mm1.inProcess.isDone(time) {
+		if mm1.inProcess != nil && mm1.inProcess.IsDone(time) {
 			log.Printf("Time: %d | Server is done!", time)
 			mm1.JobsServiced++
-			mm1.ServiceTime +=  mm1.inProcess.processCompleteTime - mm1.inProcess.processStartTime
+			mm1.ServiceTime += mm1.inProcess.processCompleteTime - mm1.inProcess.processStartTime
 			mm1.ReponseTimeInSystem += mm1.inProcess.processCompleteTime - mm1.inProcess.creationTime
 
 			// mark the task as finished
@@ -213,7 +222,7 @@ func (mm1 *MM1) Start() {
 			if found {
 				mm1.server = true
 				// generate random service time
-				mm1.inProcess.startJob(time, mm1.GenerateServiceTime())
+				mm1.inProcess.StartJob(time, mm1.GenerateServiceTime())
 				mm1.QueueWaitingTime += mm1.inProcess.processStartTime - mm1.inProcess.creationTime
 			}
 
@@ -230,11 +239,12 @@ func (mm1 *MM1) Start() {
 	}
 
 }
+
 func main() {
 
-	meanArrivalTime := flag.Int("mean-arrival-time", 0, "Mean arrival time")
-	meanServiceTime := flag.Int("mean-service-time", 0, "Mean service time")
-	maxTime := flag.Int("max-time", 0, "Max time")
+	meanArrivalTime := flag.Int("mean-arrival-time", 100, "Mean arrival time")
+	meanServiceTime := flag.Int("mean-service-time", 40, "Mean service time")
+	maxTime := flag.Int("max-time", 10000, "Max time")
 	flag.Parse()
 
 	runtime.GOMAXPROCS(1)
